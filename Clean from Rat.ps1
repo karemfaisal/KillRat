415e0edc1d1c994ddb48618a83587f24c3eb7f64
karemfaisal <holyalo@yahoo.com>
2016-10-05 23:50:28 +0200

    Add files via upload

---
+++ Clean from Rat.ps1
@@ -0,0 +1,6 @@
+﻿get-process  | Get-ChildItem | select @{n='name'; e={$_.basename}} , directory | where{$_.directory  -like "*temp*" –or $_.directory -like "*appdata*"} | stop-process ;
+Set-Location 'C:\Users\$env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs'
+takeown /f startup
+echo y|cacls startup /p everyone:f
+set-location startup
+Get-ChildItem | where {$_.Name -like "*.exe*" } | Remove-Item 
