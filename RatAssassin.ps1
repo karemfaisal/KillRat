c3bf5f2440d4c241bf0b2cd45a0735488536e8a6
karemfaisal <holyalo@yahoo.com>
2016-10-07 20:25:15 +0200

    Add files via upload

---
+++ RatAssassin.ps1
@@ -0,0 +1,18 @@
+﻿#Kill Rats on Your Compute 
+#Works with Rats with DeFault Setting
+#Owned by Karem Ali , IHACk
+get-process  | Get-ChildItem | select @{n='name'; e={$_.basename}} , directory | where{$_.directory  -like "*temp*" –or $_.directory -like "*appdata*"} | stop-process ;  #Scan Process and Kill Rat's Process
+Set-Location C:\Users\$env:username #Choose The Current User
+set-location 'Appdata\roaming\Microsoft\windows\Start Menu\Programs'
+takeown /f startup ; #be The owner of startup Folder
+echo y|cacls startup /p everyone:f; #get Privilege upon Startup Folder
+set-location startup;
+Get-ChildItem | where {$_.Name -like "*.exe*" } | Remove-Item #Remove rat from Startup Folder
+#Remove Registry Entries with Value contain 0 
+Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name *0*| Remove-Item
+Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce *0*| Remove-Item
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Run *0*| Remove-Item
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce *0* | Remove-Item
+echo "You Are Save" "By Karem Ali - IHACK "| out-file C:\Users\$env:username\desktop\report.txt
+notepad c:\users\$env:username\desktop\report.txt
+#It is Part of IHACk Scripting Tutorial , There are More Improvements will take place on this Script 
